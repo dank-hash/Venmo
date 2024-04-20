9f4ed2b558256a14810bb1725dbd422e8829499d
brickly26 <sabburak26@gmail.com>
2024-04-20 13:49:11 -0400

    Initial Commit

---
+++ Venmo/VenmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenmoApp.swift
+//  Venmo
+//
+//  Created by Burak Aksu on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VenmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
