ecb5e25a7e5886c5b092d5b3718048b98dc5b78f
brickly26 <sabburak26@gmail.com>
2024-04-23 11:39:23 -0400

    settings page

---
+++ Venmo/Screens/Categories.swift
@@ -0,0 +1,21 @@
+//
+//  Categories.swift
+//  Venmo
+//
+//  Created by Burak Aksu on 4/22/24.
+//
+
+import SwiftUI
+
+struct Categories: View {
+    var body: some View {
+        Text("Hello, Categories")
+    }
+}
+
+struct Categories_Previews: PreviewProvider {
+    static var previews: some View {
+        Categories()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+    }
+}
